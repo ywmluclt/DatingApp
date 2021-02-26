6fd2fa515d76ef88292c3bed6f2841a40d060c8e
Luc Le <ywm.luclt@gmail.com>
2021-02-26 18:22:54 +0700

    Course 6 not done

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,28 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {map} from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/auth/';
 
-constructor(private http: HttpClient) { }
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
-login(model: any){
-  return this.http.post(this.baseUrl + 'login',model).pipe(
-    map((response:any) =>{
-      const user =response;
-      if(user){
-        localStorage.setItem('token', user);
-      }
-    })
-  )
-}
+  constructor(private http: HttpClient) {}
 
-register(model: any){
-  return this.http.post(this.baseUrl + 'register',model);
-}
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        }
+      })
+    );
+  }
 
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token === null ? undefined : token);
+  }
 }
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -5,23 +5,35 @@ import { AuthService } from '../_services/auth.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  login() {
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success('Logged in successfully');
+      },
+      (error) => {
+        this.alertify.error(error);
+      }
+    );
   }
 
-  login(){
-    this.authService.login(this.model).subscribe(next=>{
-      this.alertify.success('Logged in successfully');
-    }, error =>{
-      this.alertify.error(error);
-    }
-      );
+  loggedIn() {
+    return this.authService.loggedIn();
   }
 
+  logout() {
+    localStorage.removeItem('token');
+    this.alertify.message('Logged out');
+  }
 }
